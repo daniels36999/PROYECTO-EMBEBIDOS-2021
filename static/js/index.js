c5477106075279738aff9618e033f14b895cae3f
Daniels <90793016+daniels36999@users.noreply.github.com>
2021-09-26 14:11:29 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -45,13 +45,14 @@ function LED3_Off(){
 	client.send(message);
 	console.log("LED 3 APAGADO");
 }
-
+vizualizar="HOLA"
 //FUNCION DEL BOTON MOSTRAR HISTORIAL
 function Histo1(){
 	message = new Paho.MQTT.Message("MOSTRAR_HISTORIAL");
 	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
 	client.send(message);
 	console.log("Se Mostrara el Historial");
+	vizualizar="MUESTRA"
 }
 
 //FUNCION DEL BOTON OCULTAR HISTORIAL
@@ -60,6 +61,7 @@ function Histo2(){
 	message.destinationName ="dyautibug.fie@unach.edu.ec/test1";
 	client.send(message);
 	console.log("Se Ocultara el Historial");
+	vizualizar="OCULTA"
 }
 
 // Create a client instance
@@ -115,6 +117,14 @@ function Histo2(){
 	  }
 	  if(sms=="Baja"){
 	  	document.getElementById("sensor1").innerHTML="Temperatura: "+sms;
+	  }
+	  if(vizualizar=="MUESTRA"){
+	  	document.getElementById("historial").innerHTML=sms;
+		vizualizar="HOLA"
+	  }
+	  if(vizualizar=="OCULTA"){
+	  	document.getElementById("historial").innerHTML="------------------";
+		vizualizar="HOLA"
 	  }
 
   }
